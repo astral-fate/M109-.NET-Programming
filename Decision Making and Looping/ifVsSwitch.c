3dcc2b860c27329367c0fb5162bdcf43a2538652
Fatimah Emad El-Den <63984422+astral-fate@users.noreply.github.com>
2022-10-23 00:00:20 +0300

    Create ifVsSwitch.c

---
+++ Decision Making and Looping/ifVsSwitch.c
@@ -0,0 +1,20 @@
+switch (day){
+  case 1: Console.WriteLine("Sunday"); break;
+  case 2: Console.WriteLine("Monday"); break;
+  case 3: Console.WriteLine("Tuesday"); break;
+  case 4: Console.WriteLine("Wednesday"); break;
+  default: Console.WriteLine("Try again"); 
+    break;
+}
+
+
+
+if (day == 1) 
+  Console.WriteLine("Sunday ");
+else if (day == 2) 
+  Console.WriteLine("Monday ");
+else if (day == 3) 
+  Console.WriteLine("Tuesday "); 
+else if (day == 4) 
+  Console.WriteLine("Wednesday ");
+else Console.WriteLine();
