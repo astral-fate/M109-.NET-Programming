56c82d085b7bccb35c19e3384b6ab5c184a60122
Fatimah Emad El-Den <63984422+astral-fate@users.noreply.github.com>
2022-10-25 23:48:38 +0300

    Create NestedLoop.c

---
+++ Decision Making and Looping/NestedLoop.c
@@ -0,0 +1,52 @@
+using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+namespace Sololearn
+
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+           for (int i=1;i<= 3; ++i){
+
+                for (int j=1;j<= 3; ++j){
+
+                
+
+                    Console.Write("*");
+
+                    
+
+                    }
+
+                    
+
+             Console.WriteLine("-");
+
+ 
+
+            } 
+
+        }
+
+    
+
+   }
+
+    
+
+}
+
