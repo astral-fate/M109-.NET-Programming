7a976a45e221c54dcb562b711ccdb8f86468b942
Fatimah Emad El-Den <63984422+astral-fate@users.noreply.github.com>
2022-11-02 11:40:45 +0300

    Create PRINT THE EVEN ELEMENTS OF AN ARRAY.java

---
+++ Arrays/PRINT THE EVEN ELEMENTS OF AN ARRAY.java
@@ -0,0 +1,23 @@
+public class YourClassNameHere {
+    public static void main(String[] args) {
+      
+      
+
+            int[] A = { 2,7,1,5,6,9,12,15,11,10 };
+
+            int i;
+
+            for (i=1;i<10;i++) {
+
+                  if(A[i]%2 == 0){
+
+                 System.out.println(A[i]+" ");
+
+
+              }
+
+          }
+
+      }
+
+}
