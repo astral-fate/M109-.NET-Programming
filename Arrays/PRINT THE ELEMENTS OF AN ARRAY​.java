7af34a0bc1f3fbefe9fbdf9808b43d7a43b8c919
Fatimah Emad El-Den <63984422+astral-fate@users.noreply.github.com>
2022-11-02 11:35:44 +0300

    Create PRINT THE ELEMENTS OF AN ARRAY​.java

---
+++ Arrays/PRINT THE ELEMENTS OF AN ARRAY​.java
@@ -0,0 +1,19 @@
+public class YourClassNameHere {
+    public static void main(String[] args) {
+      
+      
+
+      int A []  = { 2,7,1,5,6,9,12,15,11,10 };
+
+      int i;
+
+
+
+            for (i=1;i<10;i++)
+
+                  System.out.println(A[i]+" ");
+
+             System.out.println();
+
+        }
+    }
