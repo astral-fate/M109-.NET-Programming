03d5de1da229e2c89ea3e050af5e148dfef8ce90
Fatimah Emad El-Den <63984422+astral-fate@users.noreply.github.com>
2022-11-06 02:05:38 +0300

    Update Q2.c

--- TMA/Q2.c
+++ TMA/Q2.c
@@ -1,1 +1,21 @@
 
+print("enter seat number (-1 to end)");
+#user = input()
+user = input
+count = 0
+arr = []
+mylist=[user]
+
+
+for i in mylist:
+
+    if i ==-1:
+        continue
+    if (i<=10):
+        count =count +1
+        arr=True
+        
+        
+
+fees = count *500
+print("The flight has" , count , "reserved seats with total income" , fees )        
