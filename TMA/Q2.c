3ed9492906ac1d1b20e421ffabcd85194895ad3d
Fatimah Emad El-Den <63984422+astral-fate@users.noreply.github.com>
2022-11-06 02:11:42 +0300

    Update Q2.c

--- TMA/Q2.c
+++ TMA/Q2.c
@@ -1,7 +1,6 @@
-
 print("enter seat number (-1 to end)");
 #user = input()
-user = input
+user = 3
 count = 0
 arr = []
 mylist=[user]
@@ -14,8 +13,13 @@ for i in mylist:
     if (i<=10):
         count =count +1
         arr=True
-        
+    print("enter the weight of the bag")
+    weight = 15
+    if (weight>10):
+        extra = weight -10
+        sum = extra * 5
         
 
 fees = count *500
-print("The flight has" , count , "reserved seats with total income" , fees )        
+print("The flight has" , count , "reserved seats with total income" , fees+sum )        
+print("the extra fee is:", sum)
