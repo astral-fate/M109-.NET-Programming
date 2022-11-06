a2c29dfcf4f40a6a677784f6c6c2c119c9e71d6c
Fatimah Emad El-Den <63984422+astral-fate@users.noreply.github.com>
2022-11-06 04:39:00 +0300

    Update Q2.py

--- TMA/Q2.py
+++ TMA/Q2.py
@@ -5,9 +5,11 @@ extra =0
 fees = 0
 total = 0
 count =0
+i =0
  
 
-while(True):    
+while(True): 
+    
     print("enter seat number (-1 to end):");
     user = int(input())
     mylist=[user]  
@@ -16,43 +18,45 @@ while(True):
       break 
   
 
-
-    for i in mylist:    
-        if (i<=10):
-            count =count +1
+    if i in range(1,10):  
+        for i in mylist:    
+            if (i<=10):
+                count =count +1
+            
+            
+            print("enter the weight of the bag:")
+            weight = int(input())
+            
+            if (weight==10 or weight<=10):
+             extra = weight -10
+             fees = extra * 5
+             total = (count * 500 )+ fees   
+             print("Reservation is confirmed for seat no. ", i)
+             print("Fees of extra weight:", fees, "$")
+             print("Total fees: ", total ,"$")
+            
+            
+    
+            if (weight>10 and weight<25 ):
+             extra = weight -10
+             fees = extra * 5
+             total = (count * 500 )+ fees
+            
+            
+             print("Reservation is confirmed for seat no. ", i)
+             print("Fees of extra weight:", fees, "$")
+             print("Total fees: ", total ,"$")
+            
+            
+            
+            elif (weight>=25 or weight==25 ):
+            
+             print("Reservation cancelled due to unallowed baggage weight")
+             
+             
+             
         
-        
-        print("enter the weight of the bag:")
-        weight = int(input())
-        
-        if (weight==10 or weight<=10):
-         extra = weight -10
-         fees = extra * 5
-         total = (count * 500 )+ fees   
-         print("Reservation is confirmed for seat no. ", i)
-         print("Fees of extra weight:", fees, "$")
-         print("Total fees: ", total ,"$")
-        
-        
-
-        if (weight>10 and weight<25 ):
-         extra = weight -10
-         fees = extra * 5
-         total = (count * 500 )+ fees
-        
-        
-         print("Reservation is confirmed for seat no. ", i)
-         print("Fees of extra weight:", fees, "$")
-         print("Total fees: ", total ,"$")
-        
-        
-        
-        elif (weight>=25 or weight==25 ):
-        
-         print("Reservation cancelled due to unallowed baggage weight")
-         
-         
-         
-
+    else:
+        break 
 
 print("The flight has" , count , "reserved seats with total income" , total )
