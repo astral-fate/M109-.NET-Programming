7ecb26b836441beda95caf2c4f81b19e9aa7191f
Fatimah Emad El-Den <63984422+astral-fate@users.noreply.github.com>
2022-11-06 03:15:03 +0300

    Update Q2.c

--- TMA/Q2.c
+++ TMA/Q2.c
@@ -1,4 +1,4 @@
-print("enter seat number (-1 to end)");
+print("enter seat number (-1 to end):");
 #user = input()
 user = int(input())
 count = 0
@@ -13,15 +13,25 @@ for i in mylist:
     if (i<=10):
         count =count +1
         
-    print("enter the weight of the bag")
+    print("enter the weight of the bag:")
     weight = int(input())
     if (weight>10 and weight<25 ):
         extra = weight -10
         fees = extra * 5
         total = (count * 500 )+ fees
-        print("The flight has" , count , "reserved seats with total income" , total ) 
+        
+        
+        print("Reservation is confirmed for seat no. ", i)
+        print("Fees of extra weight:", fees, "$")
+        print("Total fees: ", total ,"$")
+
+ 
     
     if (weight>=25 or weight==25 ):
         
         print("Reservation cancelled due to unallowed baggage weight")
-        break
+        continue
+    
+    
+    
+print("The flight has" , count , "reserved seats with total income" , total )     
