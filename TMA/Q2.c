d11c95b2569bcc5b36be44c8210259c30be1fec9
Fatimah Emad El-Den <63984422+astral-fate@users.noreply.github.com>
2022-11-06 04:01:26 +0300

    Update Q2.c

--- TMA/Q2.c
+++ TMA/Q2.c
@@ -1,37 +1,44 @@
-print("enter seat number (-1 to end):");
+
 #user = input()
-user = int(input())
-count = 0
-arr = []
-mylist=[user]
 
-
-for i in mylist:
-
-    if i ==-1:
-        continue
-    if (i<=10):
-        count =count +1
-        
-    print("enter the weight of the bag:")
-    weight = int(input())
-    if (weight>10 and weight<25 ):
-        extra = weight -10
-        fees = extra * 5
-        total = (count * 500 )+ fees
-        
-        
-        print("Reservation is confirmed for seat no. ", i)
-        print("Fees of extra weight:", fees, "$")
-        print("Total fees: ", total ,"$")
-
+extra =0
+fees = 0
+total = 0
+count =0
  
-    
-    if (weight>=25 or weight==25 ):
+
+while(True):    
+    print("enter seat number (-1 to end):");
+    user = int(input())
+    mylist=[user]
+    if user ==-1:    
+      break     
+      
+    for i in mylist:
+       
+        if (i<=10):
+         count =count +1
         
-        print("Reservation cancelled due to unallowed baggage weight")
-        continue
+        print("enter the weight of the bag:")
+        weight = int(input())
+        if (weight>10 and weight<25 ):
+         extra = weight -10
+         fees = extra * 5
+         total = (count * 500 )+ fees
+        
+        
+         print("Reservation is confirmed for seat no. ", i)
+         print("Fees of extra weight:", fees, "$")
+         print("Total fees: ", total ,"$")
+        
+        
+        
+        elif (weight>=25 or weight==25 ):
+        
+         print("Reservation cancelled due to unallowed baggage weight")
+         
+         
+         
+
     
-    
-    
-print("The flight has" , count , "reserved seats with total income" , total )     
+print("The flight has" , count , "reserved seats with total income" , total )
