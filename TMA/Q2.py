0356d9ad890c1e8d5c0c4cf5f06c0ce0391d908f
Fatimah Emad El-Den <63984422+astral-fate@users.noreply.github.com>
2022-11-06 05:18:43 +0300

    Update Q2.py

--- TMA/Q2.py
+++ TMA/Q2.py
@@ -5,18 +5,23 @@ total = 0
 count =0
 i =0
 arr=[]
- 
 
-for i in range(1,10): 
-    
+
+while(True):
     print("enter seat number (-1 to end):");
     user = int(input())
     mylist=[user]  
    
     if user ==-1:    
-      break 
+        break
+  
+    if (user>10):
+        break  
+     
+    for i in mylist:
+        
+        
 
-    for i in mylist:    
         if (i<=10):
             count =count +1
             #arr[i]=True
@@ -29,9 +34,8 @@ for i in range(1,10):
         weight = int(input())
         
         if (weight==10 or weight<=10):
-         extra = weight -10
-         fees = extra * 5
-         total = (count * 500 )+ fees   
+         extra = 0
+         total = (count * 500 ) 
          print("Reservation is confirmed for seat no. ", i)
          print("Fees of extra weight:", fees, "$")
          print("Total fees: ", total ,"$")
