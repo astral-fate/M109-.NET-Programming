cc3c17366be7c6b0dbab731cc0fc712a594743ac
Fatimah Emad El-Den <63984422+astral-fate@users.noreply.github.com>
2022-11-17 23:44:05 +0300

    Create Dowhile.c

---
+++ Decision Making and Looping/Dowhile.c
@@ -0,0 +1,68 @@
+using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+
+
+namespace SoloLearn
+
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+        
+
+        
+
+        
+
+            int maxBid = Convert.ToInt32(Console.ReadLine());
+
+            int bid = int.Parse(Console.ReadLine());
+
+
+
+            //your code goes here
+
+            while(bid<maxBid){
+
+            
+
+              bid = int.Parse(Console.ReadLine());  
+
+            }
+
+            
+
+            
+
+            
+
+            Console.WriteLine("Sold: "+ bid);
+
+            
+
+           }
+
+        }
+
+        
+
+        
+
+        
+
+    }
+
