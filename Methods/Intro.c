0c5ec5a61e938a76c42fcb0b2c1f2b1c98b1926d
Fatimah Emad El-Den <63984422+astral-fate@users.noreply.github.com>
2022-11-19 23:59:50 +0300

    Create Intro.c

---
+++ Methods/Intro.c
@@ -0,0 +1,45 @@
+using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+
+
+namespace SoloLearn
+
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+
+
+            //call the method
+
+            myFunc();
+
+        }
+
+        static void myFunc()
+
+        {
+
+            //complete the method to output "This is my first method"
+
+            Console.WriteLine("This is my first method");
+
+        }
+
+    }
+
+}
